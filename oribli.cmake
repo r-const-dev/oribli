7324ae664558bd0dd3675a770c6521e8c48c9232
Razvan Constantin <r.const.dev@gmail.com>
2023-07-10 02:09:15 +0200

    Extracted embedding into a cmake function

--- oribli.cmake
+++ oribli.cmake
@@ -3,3 +3,14 @@ macro(oribli_standard)
   set(CMAKE_CXX_STANDARD_REQUIRED ON)
   set(BUILD_SHARED_LIBS OFF)
 endmacro()
+
+function(target_oribli_embed)
+  cmake_parse_arguments(ORIBLI_EMBED "" "LIB;HDR;SRC;MAP" "DEPS" ${ARGN})
+  add_custom_command(
+      OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${ORIBLI_EMBED_SRC} ${CMAKE_CURRENT_BINARY_DIR}/${ORIBLI_EMBED_HDR} 
+      DEPENDS "${ORIBLI_EMBED_DEPS}"
+      COMMAND oribli
+      ARGS embed --hdr=${ORIBLI_EMBED_HDR} --src=${ORIBLI_EMBED_SRC} --map=${ORIBLI_EMBED_MAP} "${ORIBLI_EMBED_DEPS}")
+  target_sources(${ORIBLI_EMBED_LIB} PRIVATE ${CMAKE_CURRENT_BINARY_DIR}/${ORIBLI_EMBED_SRC} PUBLIC ${CMAKE_CURRENT_BINARY_DIR}/${ORIBLI_EMBED_HDR})
+  target_include_directories(${ORIBLI_EMBED_LIB} INTERFACE ${CMAKE_CURRENT_BINARY_DIR})
+endfunction()
